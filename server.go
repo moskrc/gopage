dbd55bb340f96c5cb6fc75e6b536f407a37bd6e9
Vitaliy Shishorin <moskrc@gmail.com>
2014-07-06 09:15:07 +0400

    package utils

--- server.go
+++ server.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"errors"
+	"./utils"
 	"fmt"
 	"github.com/codegangsta/martini-contrib/binding"
 	"github.com/codegangsta/martini-contrib/render"
@@ -10,30 +9,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"net/smtp"
-	"reflect"
-	"runtime"
-	"strings"
 	"time"
 )
-
-func IsSet(a interface{}, key interface{}) bool {
-	av := reflect.ValueOf(a)
-	kv := reflect.ValueOf(key)
-
-	switch av.Kind() {
-	case reflect.Array, reflect.Chan, reflect.Slice:
-		if int64(av.Len()) > kv.Int() {
-			return true
-		}
-	case reflect.Map:
-		if kv.Type() == av.Type().Key() {
-			return av.MapIndex(kv).IsValid()
-		}
-	}
-
-	return false
-}
 
 type Feedback struct {
 	Created int64
@@ -66,7 +43,7 @@ func main() {
 		Layout: "layout",
 
 		Funcs: []template.FuncMap{
-			{"isset": IsSet}, // example
+			{"isset": utils.IsSet}, // example
 		},
 	}))
 
@@ -77,6 +54,9 @@ func main() {
 	})
 
 	m.Get("/", func(r render.Render, ctx *ContextProcessor) {
+		fmt.Println(utils.A)
+		utils.A = 3
+		fmt.Println(utils.A)
 		resp := map[string]interface{}{"ctx": ctx}
 		r.HTML(200, "index", resp)
 	})
@@ -112,84 +92,9 @@ func SendFeedback(errors binding.Errors, ctx *ContextProcessor, f Feedback, c ma
 	fmt.Printf("%+v", errors)
 	nf := newFeedback(f.Email, f.Body)
 
-	SendEmail(
-		"smtp.gmail.com",
-		587,
-		nf.Email,
-		"xxx",
-		[]string{"xxx@xxx.com"},
-		"Новый запрос",
-		nf.Body)
+	utils.SendEmail("smtp.gmail.com", 587, nf.Email, "xxx", []string{"xxx@xxx.com"}, "Новый запрос", nf.Body)
 
 	resp := map[string]interface{}{"ctx": ctx, "nf": nf, "success": true, "errors": errors}
 
 	r.HTML(200, "feedback", resp)
-}
-
-func _CatchPanic(err *error, functionName string) {
-	if r := recover(); r != nil {
-		fmt.Printf("%s : PANIC Defered : %v\n", functionName, r)
-
-		// Capture the stack trace
-		buf := make([]byte, 10000)
-		runtime.Stack(buf, false)
-
-		fmt.Printf("%s : Stack Trace : %s", functionName, string(buf))
-
-		if err != nil {
-			*err = errors.New(fmt.Sprintf("%v", r))
-		}
-	} else if err != nil && *err != nil {
-		fmt.Printf("%s : ERROR : %v\n", functionName, *err)
-
-		// Capture the stack trace
-		buf := make([]byte, 10000)
-		runtime.Stack(buf, false)
-
-		fmt.Printf("%s : Stack Trace : %s", functionName, string(buf))
-	}
-}
-
-func SendEmail(host string, port int, userName string, password string,
-	to []string, subject string, message string) (err error) {
-	defer _CatchPanic(&err, "SendEmail")
-
-	parameters := &struct {
-		From    string
-		To      string
-		Subject string
-		Message string
-	}{
-		userName,
-		strings.Join([]string(to), ","),
-		subject,
-		message,
-	}
-
-	buffer := new(bytes.Buffer)
-
-	template := template.Must(template.New("emailTemplate").Parse(_EmailScript()))
-	template.Execute(buffer, parameters)
-
-	auth := smtp.PlainAuth("", userName, password, host)
-
-	err = smtp.SendMail(
-		fmt.Sprintf("%s:%d", host, port),
-		auth,
-		userName,
-		to,
-		buffer.Bytes())
-
-	return err
-}
-
-// _EmailScript returns a template for the email message to be sent
-func _EmailScript() (script string) {
-	return `From: {{.From}}
-To: {{.To}}
-Subject: {{.Subject}}
-MIME-version: 1.0
-Content-Type: text/html; charset="UTF-8"
-
-{{.Message}}`
 }
---
+++ utils/stuff.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"reflect"
+)
+
+var A = 2
+
+func IsSet(a interface{}, key interface{}) bool {
+	av := reflect.ValueOf(a)
+	kv := reflect.ValueOf(key)
+
+	switch av.Kind() {
+	case reflect.Array, reflect.Chan, reflect.Slice:
+		if int64(av.Len()) > kv.Int() {
+			return true
+		}
+	case reflect.Map:
+		if kv.Type() == av.Type().Key() {
+			return av.MapIndex(kv).IsValid()
+		}
+	}
+
+	return false
+}
